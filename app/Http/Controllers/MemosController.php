c982331f4c581efc578dcfa22d9bee953162af94
Koichiro Rinsaka <rinsaka@ba.kobegakuin.ac.jp>
2019-08-30 14:07:26 +0900

    comments memos @ index の雛形

--- app/Http/Controllers/MemosController.php
+++ app/Http/Controllers/MemosController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class MemosController extends Controller
 {
-  //
+  public function index()
+  {
+    dd('Memos');
+  }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,12 +83,8 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                  <a href="{{ action('CommentsController@index') }}">Comments</a>
+                  <a href="{{ action('MemosController@index') }}">Memos</a>
                 </div>
             </div>
         </div>
