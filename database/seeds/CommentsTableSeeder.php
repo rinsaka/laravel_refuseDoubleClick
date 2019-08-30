a7d0c4127a191cd88fd6b43cd63c4faed3422667
Koichiro Rinsaka <rinsaka@ba.kobegakuin.ac.jp>
2019-08-30 14:01:57 +0900

    seeder

--- database/seeds/CommentsTableSeeder.php
+++ database/seeds/CommentsTableSeeder.php
@@ -4,13 +4,18 @@ use Illuminate\Database\Seeder;
 
 class CommentsTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        //
-    }
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    DB::table('comments')->delete();
+
+    DB::table('comments')->insert([
+      'comment' => 'フォームのダブルクリックを拒否したい'
+    ]);
+
+  }
 }
--- database/seeds/MemosTableSeeder.php
+++ database/seeds/MemosTableSeeder.php
@@ -4,13 +4,17 @@ use Illuminate\Database\Seeder;
 
 class MemosTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        //
-    }
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    DB::table('memos')->delete();
+
+    DB::table('memos')->insert([
+      'memo' => '複数のフォームでも効率的に拒否したい'
+    ]);
+  }
 }
