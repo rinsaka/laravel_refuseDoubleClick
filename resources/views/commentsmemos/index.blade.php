7aa900ff68733ffc754f48215374f8d53c104e14
Koichiro Rinsaka <rinsaka@ba.kobegakuin.ac.jp>
2019-08-30 15:33:04 +0900

    複数のフォームで拒否

--- resources/views/commentsmemos/index.blade.php
+++ resources/views/commentsmemos/index.blade.php
@@ -3,22 +3,36 @@
 <head>
   <meta charset='utf-8'>
   <title>Comments</title>
+
+  <script>
+  function checkDoubleClick() {
+    var elems = document.getElementsByClassName('doubleClickDisabled');
+    if (elems[0].disabled) {
+      return false;
+    } else {
+      for (var i = 0; i < elems.length; i++) {
+        elems[i].disabled = true;
+      }
+      return true;
+    }
+  }
+  </script>
 </head>
 <body>
   <h1>複数のフォーム</h1>
   <p>
-    ページ内に複数のフォームがあるときに，効率的にダブルクリックを拒否したい．
+    ページ内に複数のフォームがあるときに，効率的にダブルクリックを拒否したい．ポイントは「投稿」ボタンすべてに同じ名前のクラスを設定し，Javascript でそのクラスをベースに処理する．
   </p>
   <h2>Comments</h2>
   <div>
-    <form method="post" action="{{ url('/commentsmemos/comments') }}">
+    <form method="post" action="{{ url('/commentsmemos/comments') }}" onsubmit="return checkDoubleClick();">
         @csrf
         <p>
             <label for="comment">Comment: </label>
             <input type="text" name="comment" id="comment" value="" required>
         </p>
         <p>
-            <input type="submit" value="投稿">
+            <input type="submit" value="投稿" class="doubleClickDisabled">
         </p>
     </form>
   </div>
@@ -30,14 +44,14 @@
 
   <h2>Memos</h2>
   <div>
-    <form method="post" action="{{ url('/commentsmemos/memos') }}">
+    <form method="post" action="{{ url('/commentsmemos/memos') }}" onsubmit="return checkDoubleClick();">
         @csrf
         <p>
             <label for="memo">Memo: </label>
             <input type="text" name="memo" id="memo" value="" required>
         </p>
         <p>
-            <input type="submit" value="投稿">
+            <input type="submit" value="投稿" class="doubleClickDisabled">
         </p>
     </form>
   </div>
