1bf00167cae04e1e0444e5160ce2c7a32ecb63d4
Koichiro Rinsaka <rinsaka@ba.kobegakuin.ac.jp>
2019-08-30 15:00:43 +0900

    memos はダブルクリック拒否

--- resources/views/memos/index.blade.php
+++ resources/views/memos/index.blade.php
@@ -3,21 +3,34 @@
 <head>
   <meta charset='utf-8'>
   <title>Memos</title>
+
+  <script>
+  function checkDoubleClick() {
+    var elem = document.getElementById('btn_submit');
+    if (elem.disabled) {
+      return false;
+    } else {
+      elem.disabled = true;
+      return true;
+    }
+  }
+  </script>
 </head>
 <body>
   <h1>Memos</h1>
   <p>
-    このページでは「投稿」ボタンをダブルクリックすると２重に投稿可能です．
+    このページでは「投稿」ボタンをダブルクリックを拒否します．
   </p>
+
   <div>
-    <form method="post" action="{{ url('/memos') }}">
+    <form method="post" action="{{ url('/memos') }}" onsubmit="return checkDoubleClick();">
         @csrf
         <p>
             <label for="memo">Comment: </label>
             <input type="text" name="memo" id="memo" value="" required>
         </p>
         <p>
-            <input type="submit" value="投稿">
+            <input type="submit" value="投稿" id="btn_submit">
         </p>
     </form>
   </div>
